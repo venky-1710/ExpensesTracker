61f55e48b68227eaf50403bc0ed1fedb34574464
VenkateswararaoSurampudi <VSurampudi429@everestdx.com>
2026-06-28 02:15:48 +0530

    added new dependency and updated the gitignore file

--- mobile/src/components/animated-icon.tsx
+++ mobile/src/components/animated-icon.tsx
@@ -1,8 +1,7 @@
 import { Image } from 'expo-image';
 import { useState } from 'react';
 import { Dimensions, StyleSheet, View } from 'react-native';
-import Animated, { Easing, Keyframe } from 'react-native-reanimated';
-import { scheduleOnRN } from 'react-native-worklets';
+import Animated, { Easing, Keyframe, runOnJS } from 'react-native-reanimated';
 
 const INITIAL_SCALE_FACTOR = Dimensions.get('screen').height / 90;
 const DURATION = 600;
@@ -36,7 +35,7 @@ export function AnimatedSplashOverlay() {
       entering={splashKeyframe.duration(DURATION).withCallback((finished) => {
         'worklet';
         if (finished) {
-          scheduleOnRN(setVisible, false);
+          runOnJS(setVisible)(false);
         }
       })}
       style={styles.backgroundSolidColor}
